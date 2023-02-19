dced092cf78025a42fb272ab82f3181cf86fb52b
kirhal <kirillgaleckiy@gmail.com>
2023-02-19 17:04:51 +0200

    1-st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,50 +1,28 @@
 import { galleryItems } from './gallery-items';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const galleryBoxEl = document.querySelector('.gallery');
 
 function createListItemsMarkup(obj) {
   return obj
     .map(
-      ({ preview, original, description }) => `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`
+      ({ preview, original, description }) => `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
     )
     .join('');
 }
 
 galleryBoxEl.innerHTML = createListItemsMarkup(galleryItems);
 
-galleryBoxEl.addEventListener('click', onGalleryBoxClick);
-
-function onGalleryBoxClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const onEcsClick = e => {
-    if (e.code === 'Escape') {
-      instance.close();
-    }
-  };
-
-  const instance = basicLightbox.create(
-    `<img src="${e.target.dataset.source}" width="1280" height="855">`,
-    {
-      onShow: instance => {
-        window.addEventListener('keydown', onEcsClick);
-      },
-      onClose: instance => {
-        window.removeEventListener('keydown', onEcsClick);
-      },
-    }
-  );
-  instance.show();
-}
+let gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  scrollZoom: false,
+  captionType: 'attr',
+  captionSelector: 'img',
+  captionsData: 'alt',
+  docClose: false,
+});
