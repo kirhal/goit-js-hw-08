e5b2741e37136a23a4add8fdfd3197a8f057d80b
kirhal <kirillgaleckiy@gmail.com>
2023-02-19 18:59:18 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player', {
+  width: 640,
+  byline: false,
+  controls: false,  
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
