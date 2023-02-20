bb356ff6fac59752b45bf34b09913f52745d6122
kirhal <kirillgaleckiy@gmail.com>
2023-02-20 15:30:33 +0200

    3rd done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const feedbackFormEl = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const localStoargeHasValue = localStorage.getItem(LOCALSTORAGE_KEY);
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const objectFromLocalStorage = JSON.parse(localStoargeHasValue);
+
+if (localStoargeHasValue) {
+  feedbackFormEl.elements.email.value = objectFromLocalStorage.email;
+  feedbackFormEl.elements.message.value = objectFromLocalStorage.message;
+}
+
+feedbackFormEl.addEventListener('input', throttle(onFormFillInput, 500));
+feedbackFormEl.addEventListener('submit', onSubmitButtonCkick);
+
+function onFormFillInput(e) {
+  if (e.target.nodeName === 'INPUT') formData.email = e.target.value;
+  else {
+    formData.message = e.target.value;
+  }
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitButtonCkick(e) {
+  e.preventDefault();
+  console.log(objectFromLocalStorage);
+  feedbackFormEl.elements.email.value = '';
+  feedbackFormEl.elements.message.value = '';
+  localStorage.clear();
+}
